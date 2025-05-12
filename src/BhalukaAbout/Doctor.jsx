647a1d46da86f79af7798e44887a041448041b44
Nazmol3215 <mdasikahamad2006@gmail.com>
2025-05-12 06:13:59 +0600

    Add11 vercel.json to set dist as output directory

--- src/BhalukaAbout/Doctor.jsx
+++ src/BhalukaAbout/Doctor.jsx
@@ -42,14 +42,14 @@ const ProfileCard = ({ name, mobile, email, location }) => {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    border: '1px solid #ddd',
+    border: '13px solid #ddd',
     borderRadius: '10px',
     padding: '20px',
     width: '300px',
     boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
     backgroundColor: '#f9f9f9',
     fontFamily: 'Arial, sans-serif',
-    margin: '10px',
+    margin: '140px',
   };
 
   return (
